22a875b12b7d5adcd5a5826c7d0bde0f173e6de5
yangtongbin <yang_t_b@qq.com>
2018-09-15 15:53:56 +0800

    框架重构

--- open-care-core/src/main/java/com/sm/open/care/core/exception/BizRuntimeException.java
+++ open-care-core/src/main/java/com/sm/open/care/core/exception/BizRuntimeException.java
@@ -6,25 +6,25 @@ public class BizRuntimeException extends RuntimeException {
 	private static final long serialVersionUID = 3064458242669384355L;
 
 	private final String errorCode;
-	private final String msg;
+	private final String message;
 	private Throwable throwable;
 
 	public BizRuntimeException(String message){
 		this(StringUtils.EMPTY, message, null);
 	}
 
-	public BizRuntimeException(String errorCode, String msg) {
+	public BizRuntimeException(String errorCode, String message) {
 		this.errorCode = errorCode;
-		this.msg = msg;
+		this.message = message;
 	}
 
 	public BizRuntimeException(String message, Throwable throwable){
 		this(StringUtils.EMPTY, message, throwable);
 	}
 
-	public BizRuntimeException(String errorCode, String msg, Throwable throwable) {
+	public BizRuntimeException(String errorCode, String message, Throwable throwable) {
 		this.errorCode = errorCode;
-		this.msg = msg;
+		this.message = message;
 		this.throwable = throwable;
 	}
 
@@ -32,16 +32,17 @@ public class BizRuntimeException extends RuntimeException {
 		return this.errorCode;
 	}
 
+	@Override
 	public String getMessage() {
 		Throwable cause = getCause();
-		String message = this.msg;
+		String message = this.message;
 		if (cause != null) {
 			StringBuilder sb = new StringBuilder();
 			if (message != null) {
 				sb.append("【errorCod】:").append(this.errorCode).append("; ");
 			}
 			if (message != null) {
-				sb.append(this.msg).append("; ");
+				sb.append(this.message).append("; ");
 			}
 			sb.append("nested exception is ").append(cause);
 			return sb.toString();
--- open-care-core/src/main/java/com/sm/open/care/core/utils/BeanUtil.java
+++ open-care-core/src/main/java/com/sm/open/care/core/utils/BeanUtil.java
@@ -1,11 +1,10 @@
 package com.sm.open.care.core.utils;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
 import com.alibaba.fastjson.JSON;
 import com.sm.open.care.core.ErrorCode;
 import com.sm.open.care.core.ErrorMessage;
 import com.sm.open.care.core.exception.BizRuntimeException;
-import com.sm.open.care.core.log.LoggerUtil;
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -15,48 +14,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @Description: bean相关工具类
+ * @ClassName: BeanUtil
+ * @Description: bean转换工具类
+ * @Author yangtongbin
+ * @Date 2018/2/2 09:31
  */
 public class BeanUtil {
 
-	private static final Logger logger = LoggerFactory.getLogger(BeanUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BeanUtil.class);
 
-	/**
-	 * @Description: 将 T 类型的对象数据转换成 V 类型数据
-	 * @Param:
-	 * @Return:
-	 */
-	public static <T, V> V convert(T t, Class<V> clazz) {
-		if (t == null) {
-			return null;
-		}
-		try {
-			V v = clazz.newInstance();
-			BeanUtils.copyProperties(t, v);
-			return v;
-		} catch (Exception e) {
-			LoggerUtil.info(logger, "【core-BeanUtils-convert】, {0}, t:{1}",
-					MessageFormat.format(ErrorMessage.MESSAGE_GENERAL_110008, t.getClass(), clazz), JSON.toJSONString(t));
-			throw new BizRuntimeException(ErrorCode.ERROR_GENERAL_110008,
-					MessageFormat.format(ErrorMessage.MESSAGE_GENERAL_110008, t.getClass(), clazz), e);
-		}
-	}
+    /**
+     * 将 T 类型的对象数据转换成 V 类型数据
+     *
+     * @param t
+     * @param clazz
+     * @param <T>
+     * @param <V>
+     * @return
+     */
+    public static <T, V> V convert(T t, Class<V> clazz) {
+        if (t == null) {
+            return null;
+        }
+        try {
+            V v = clazz.newInstance();
+            BeanUtils.copyProperties(t, v);
+            return v;
+        } catch (Exception e) {
+            LOGGER.info("【BeanUtils-convert】, {0}, t:{1}",
+                    MessageFormat.format(ErrorMessage.MESSAGE_GENERAL_110008, t.getClass(), clazz), JSON.toJSONString(t));
+            throw new BizRuntimeException(ErrorCode.ERROR_GENERAL_110008,
+                    MessageFormat.format(ErrorMessage.MESSAGE_GENERAL_110008, t.getClass(), clazz), e);
+        }
+    }
 
-	/**
-	 * @Description: 将 T 类型的数据对象数组转换成 V 类型对象的数据数组
-	 * @Param:
-	 * @Return:
-	 */
-	public static <T, V> List<V> convertList(List<T> tList, Class<V> clazz) {
-		if(CollectionUtils.isEmpty(tList)) {
-			return null;
-		}
-		List<V> vList = new ArrayList<V>(tList.size());
-		T t = null;
-		for(int i = 0; i < tList.size(); i ++) {
-			t = tList.get(i);
-			vList.add(BeanUtil.convert(t, clazz));
-		}
-		return vList;
-	}
+    /**
+     * 将 T 类型的数据对象数组转换成 V 类型对象的数据数组
+     *
+     * @param tList
+     * @param clazz
+     * @param <T>
+     * @param <V>
+     * @return
+     */
+    public static <T, V> List<V> convertList(List<T> tList, Class<V> clazz) {
+        if (CollectionUtils.isEmpty(tList)) {
+            return new ArrayList<V>(0);
+        }
+        List<V> vList = new ArrayList<V>(tList.size());
+        T t = null;
+        for (int i = 0; i < tList.size(); i++) {
+            t = tList.get(i);
+            vList.add(BeanUtil.convert(t, clazz));
+        }
+        return vList;
+    }
 }
